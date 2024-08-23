b1ce4ac2f2875ef505957e88d5b6ea1ce5906e0d
Polina Muravieva <polinamur3@gmail.com>
2024-08-23 17:40:11 +0300

    created a snake movement in all directions

--- src/js/modules/snake.js
+++ src/js/modules/snake.js
@@ -13,14 +13,30 @@ export class Snake {
 
         this.positionsCount = positionsCount;
         this.positionsSize = positionsSize;
+
+        this.addKeyboardHandler();
+    }
+
+    addKeyboardHandler() {
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'ArrowLeft' && this.currentDirection !== 'right') {
+                this.currentDirection = 'left';
+            } else if (event.key === 'ArrowRight' && this.currentDirection !== 'left') {
+                this.currentDirection = 'right';
+            } else if (event.key === 'ArrowUp' && this.currentDirection !== 'down') {
+                this.currentDirection = 'up';
+            } else if (event.key === 'ArrowDown' && this.currentDirection !== 'up') {
+                this.currentDirection = 'down';
+            }
+        })
     }
 
     showSnake() {
         for (let i = 0; i < this.snake.length; i++) {
             this.context.fillStyle = 'black';
             this.context.beginPath();
-            this.context.fillRect(this.snake[i].x * this.positionsSize,
-                this.snake[i].y * this.positionsSize, this.positionsSize, this.positionsSize);
+            this.context.fillRect(this.snake[i].x * this.positionsSize - this.positionsSize,
+                this.snake[i].y * this.positionsSize - this.positionsSize, this.positionsSize, this.positionsSize);
         }
 
         // сохраняем значения
@@ -32,13 +48,29 @@ export class Snake {
         this.snake.pop(); //удаляем последний элемент из массива, чтобы нарисовать новое местоположение
 
         if (this.currentDirection === 'left') {
-            newHeadPosition.x -= 1;
+            if (newHeadPosition.x === 1) {
+                newHeadPosition.x = this.positionsCount;
+            } else {
+                newHeadPosition.x -= 1;
+            }
         } else if (this.currentDirection === 'right') {
-            newHeadPosition.x += 1;
+            if (newHeadPosition.x === this.positionsCount) {
+                newHeadPosition.x = 1;
+            } else {
+                newHeadPosition.x += 1;
+            }
         } else if (this.currentDirection === 'up') {
-            newHeadPosition.y -= 1;
+            if (newHeadPosition.y === 1) {
+                newHeadPosition.y = this.positionsCount;
+            } else {
+                newHeadPosition.y -= 1;
+            }
         } else if (this.currentDirection === 'down') {
-            newHeadPosition.y += 1;
+            if (newHeadPosition.y === this.positionsCount) {
+                newHeadPosition.y = 1;
+            } else {
+                newHeadPosition.y += 1;
+            }
         }
 
         this.snake.unshift(newHeadPosition); //добавляет элемент в начало массива
